8bf6cd708758f8a4e738f84ed03a079b4392fe79
Hana Jirovska <H.Jirovska@student.tudelft.nl>
2019-03-09 22:06:11 +0100

    Fix helper functions, comment print command

--- hello.py
+++ hello.py
@@ -38,81 +38,75 @@ def create_app():
 app = create_app()
 
 
-# run_with_ngrok(app)
+# url = "http://130.82.239.210"
 
-url = "http://130.82.239.210"
+@app.route("/driver")
+def driver_ready():
+    data = pd.read_csv(os.path.join(config['base_dir'],config['tmp_subdir'],config['stream_filename']))
+    # print(data.shape)
+    if has_capacity(data) and calm_driving(data) and smooth_speed(data) and heart_rate_ok(data) and no_sweating(data):
+        return "True"
+    else:
+        return "False"
 
-#@app.route("/driver")
-# def driver_ready():
-#     if has_capacity() & calm_driving() & smooth_speed() & heart_rate_ok() & no_sweating():
-#         return True
-#     else:
-#         return False
-#
-#
-# # HELPER FUNCTIONS BELOW
-#
-# # determines whether the driver is not excessively sweating
-# def no_sweating(threshold):
-#     # TODO: max sweat level from last 60 seconds
-#     if max > threshold:
-#         return False
-#     else:
-#         return True
-#
-#
-# # determines whether the heart rate is in acceptable range
-# def heart_rate_ok(threshold):
-#     # TODO: average from last 60 seconds
-#     if avg > threshold:
-#         return False
-#     else:
-#         return True
-#
-#
-# # determines whether there is any space in the car
-# def has_capacity():
-#     # TODO: get data from CSV (last cell available)
-#     attributes = ['AB_Gurtschloss_BF', 'AB_Gurtschloss__Reihe2_FA', 'AB_Gurtschloss__Reihe2_MI', 'AB_Gurtschloss__Reihe2_BF']
-#     capacity = 0
-#
-#     for name in attributes:
-#         r = requests.get(url + '/signal/' + name + '/value')
-#         data = json.loads(json.dumps(r.json()))
-#         try:
-#             if data.get('measurement').get('value') == 2:
-#                 capacity += 1
-#         except:
-#             continue
-#     if capacity > 0:
-#         return True
-#     else:
-#         return False
-#
-#
-# # determines whether speed deviates by more than arbitrary threshold
-# # TODO: use speed limits on given roads
-# def smooth_speed(threshold=30):
-#     # TODO: get data from CSV, if stdv above threshold, False
-#     if stdv > threshold:
-#         return False
-#     else:
-#         return True
-#
-#
-# # determines whether the driving behaviour is calm
-# # threshold is an arbitrary value for now, could be ml
-# def calm_driving(threshold=1000):
-#     # TODO: use average values from last 60 seconds
-#     r = requests.get(url + '/signal/MO_Drehzahl_01/value')
-#     data = json.loads(json.dumps(r.json()))
-#     avg = data.get('measurement').get('value')
-#
-#     if avg > threshold:
-#         return False
-#     else:
-#         return True
 
+# HELPER FUNCTIONS BELOW
+
+# determines whether the driver is not excessively sweating
+def no_sweating(data, threshold=1, context_window=60, attr='EDA_0'):
+    context = data[attr].iloc[-context_window:]
+    max = context.max()
+    # print(max)
+    if max > threshold:
+        return False
+    else:
+        return True
+
+
+# determines whether the heart rate is in acceptable range
+def heart_rate_ok(data, threshold=60, context_window=60, attr='HR_0'):
+    context = data[attr].iloc[-context_window:]
+    avg = np.average(context)
+    if avg > threshold:
+        return False
+    else:
+        return True
+
+
+# determines whether there is any space in the car
+def has_capacity(data):
+    attributes = ['can1_AB_Gurtschloss_BF []', 'can1_AB_Gurtschloss_Reihe2_FA []', 'can1_AB_Gurtschloss_Reihe2_MI []', 'can1_AB_Gurtschloss_Reihe2_BF []']
+    capacity = 0
+
+    for name in attributes:
+        context = data[name].iloc[-1]
+        if context == 2:
+            capacity += 1
+
+    if capacity > 0:
+        return True
+    else:
+        return False
+
+
+# determines whether speed deviates by more than arbitrary threshold
+def smooth_speed(data, threshold=30, context_window=60, attr='can0_ESP_v_Signal [Unit_KiloMeterPerHour]'):
+    context = data[attr].iloc[-context_window:]
+    stdv = np.std(context)
+    if stdv > threshold:
+        return False
+    else:
+        return True
+
+
+# determines whether the driving behaviour is calm
+def calm_driving(data, threshold=1000, context_window=60, attr='can0_MO_Drehzahl_01 [Unit_MinutInver]'):
+    context = data[attr].iloc[-context_window:]
+    avg = np.average(context)
+    if avg > threshold:
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
