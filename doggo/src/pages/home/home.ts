d2b2d8f97a1a660ec68c0278c815d27f1dc854d5
Shourya Pratap Singh <shourya4@gmail.com>
2019-03-09 21:28:46 +0100

    Driver mode first page

--- doggo/src/pages/home/home.ts
+++ doggo/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { PassengerPage } from '../passenger/passenger';
+import { DriverPage } from '../driver/driver';
 
 @Component({
   selector: 'page-home',
@@ -15,6 +16,10 @@ export class HomePage {
 
   openPassengerPage() {
     this.navCtrl.push(PassengerPage)
+  }
+
+  openDriverPage() {
+    this.navCtrl.push(DriverPage)
   }
 
 }
